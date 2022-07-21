b4556ba2d2cd59b19cdb06e26ab2f2aa8cc31667
apple <rajani@apples-MacBook-Pro.local>
2022-07-21 22:44:26 +0530

    show value in movies list

--- app/src/main/java/com/wipro/model/TvShow.kt
+++ app/src/main/java/com/wipro/model/TvShow.kt
@@ -1,13 +1,18 @@
 package com.wipro.model
 
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class TvShow(
-    val country: String,
-    val end_date: Any,
-    val id: Int,
-    val image_thumbnail_path: String,
-    val name: String,
-    val network: String,
-    val permalink: String,
-    val start_date: String,
-    val status: String
-)
+    @SerializedName("country") val country: String,
+    @SerializedName("end_date") val endDate: String,
+    @SerializedName("id") val id: Int,
+    @SerializedName("image_thumbnail_path") val imageUrl: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("network") val network: String,
+    @SerializedName("permalink") val permalink: String,
+    @SerializedName("start_date") val startDate: String,
+    @SerializedName("status") val status: String
+) : Parcelable
